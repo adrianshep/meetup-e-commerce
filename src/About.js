6305f2d90ac6ed26b27a7d78fae5541d221c2ea0
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-08-10 11:18:15 -0400

    initial project commits

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Locations</h3>
+        <h5>Columbia</h5>
+        <h5>Baltimore</h5>
+        <h5>Hyattsville</h5>
+        <h3>History</h3>
+        <p>Our wonderful bakery began in 1905 in Baltimore, Maryland</p>
+        <img src= />
+        <About />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'About' from './About.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to our Bakery Site!</h1>
+        <img src= />
+        <About />
+      </div>
+    );
+  }
+}
+
+export default App;
