5883db2bbcc480be6e1b75930769ed24fa267038
Adrian Shepelavey <adrianshepelavey@gmail.com>
2018-08-12 12:04:57 -0400

    added to About.js

--- src/About.js
+++ src/About.js
@@ -8,6 +8,7 @@ class About extends Component {
         <h5>Columbia</h5>
         <h5>Baltimore</h5>
         <h5>Hyattsville</h5>
+        <h5>Timonium-Lutherville</h5>
         <h3>History</h3>
         <p>Our wonderful bakery began in 1905 in Baltimore, Maryland</p>
         <p>Founded by members of the Catonsville Nine</p>
